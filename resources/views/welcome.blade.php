2baf2cd5f49c0fd1519456240dfb5db1005e7d34
jscarton <jscarton@gmail.com>
2017-12-12 13:12:39 -0500

    fix layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>JScarton's Code Search API</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -82,5 +82,6 @@
                 </div>
             </div>
         </div>
+        <br/><br/><br/><br/><br/>
     </body>
 </html>
